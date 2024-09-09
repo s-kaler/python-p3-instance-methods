47c1c58fa61597c138e369b32530eebdaf614f90
Sundeep <sundeep.kaler@gmail.com>
2024-09-09 15:22:14 -0400

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,8 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")        
     pass
